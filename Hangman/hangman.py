1bf14ef6ad8440fdabab9040446f6d52f6b261f2
Mélanie Bigler <melanie.bigler@gmx.net>
2024-11-08 10:20:55 +0100

    version 1.1 of simplest functional Hangman game

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,8 +2,8 @@ from hangman_functions import get_word, get_letter, print_hangman, display_word
 
 if __name__ == '__main__':
 
-    print("Welcome to play hangman")
-    print("First Player:")
+    print("Welcome to play Hangman")
+    print("First Player,")
 
     #set a word to play
     word = get_word()
@@ -11,9 +11,11 @@ if __name__ == '__main__':
     print("Thank you for the word!")
     print ("Second Player can now start guessing")
 
+    #set wrong and positive guesses to 0
     pos_tries = ""
     neg_tries = ""
 
+    #player two is now starting to guess the word
     while len(neg_tries) < 9 and not all(letter in pos_tries for letter in word):
         #get a letter as guess
         guess = get_letter(pos_tries, neg_tries)
@@ -28,10 +30,10 @@ if __name__ == '__main__':
 
         # Display the number of wrong guesses
         print_hangman(len(neg_tries))
-        print("Number of wrong guesses: ", len(neg_tries))
-        print("Wrong guessed letters: ", neg_tries)
+        #print("Number of wrong guesses: ", len(neg_tries))
+        #print("Wrong guessed letters: ", neg_tries)
 
-
+    #print the winner
     print("The Game is over:")
     if len(neg_tries) < 9:
         print("Player 1 won")
